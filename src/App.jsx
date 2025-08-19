ca95fb73a758f6c4ecf0db9618fa91c0ce163948
Sara <sarailic431@gmail.com>
2025-08-19 20:51:56 +0200

    ...      ...

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-        });
-    }, 1000);
-  }, []);
-  return (
-    <div className="home">
-      {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
--- src/BlogList.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-function BlogList(props) {}
-
-export default BlogList;
