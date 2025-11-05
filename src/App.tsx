ab12713e570ecb73a6fd9125320b42b39154b71a
Sara <sarailic431@gmail.com>
2025-11-05 20:22:11 +0100

    ....................

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,38 @@ import "./App.css";
 import { expect, it } from "vitest";
 import type { Expect, Equal } from "./helpers";
 import type { Task } from "./types";
-import { error } from "console";
+
+// ///////////////// video 106
+
+// This function returns never, because it never returns!
+const getNever = () => {
+  throw new Error("This function never returns");
+};
+
+// --------------------------------------------------------------------
+
+const fn = (input: never) => {};
+
+// Nothing is assignable to never!
+fn("hello");
+fn(42);
+fn(true);
+fn({});
+fn([]);
+fn(() => {});
+
+// Except for never itself!
+
+fn(getNever());
+
+// --------------------------------------------------------------------
+
+// But we can assign never to anything
+
+const str: string = getNever();
+const num: number = getNever();
+const bool: boolean = getNever();
+const arr: string = getNever();
 
 // ///////////////// video 104  NARROWING UNKNOWN IN A LARGE CONDITIONAL STATEMENT
 
