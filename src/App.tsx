be250733ac30a7d984aa4d25cabbdf5aefd28d8d
Sara <sarailic431@gmail.com>
2025-12-08 21:47:09 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,34 @@ type User = {
 type Product = {
   id: string;
   createdAt: Date;
+  name: string;
+  price: number;
 };
+
+type tests = [
+  Expect<
+    Extends<
+      User,
+      {
+        id: string;
+        createdAt: Date;
+        name: string;
+        email: string;
+      }
+    >
+  >,
+  Expect<
+    Extends<
+      Product,
+      {
+        id: string;
+        createdAt: Date;
+        name: string;
+        price: number;
+      }
+    >
+  >
+];
 
 // ///////////////// video 124
 
