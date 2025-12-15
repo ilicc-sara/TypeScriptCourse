22c5474f41ea48efe0a240ffa734a05f33089025
Sara <sarailic431@gmail.com>
2025-12-15 23:42:08 +0100

    manage subtypes with the pick utility type, solution 2

--- src/App.tsx
+++ src/App.tsx
@@ -25,17 +25,39 @@ import type { Task } from "./types";
 
 //   type test = Expect<Equal<typeof user, { name: string; email: string }>>;
 // };
-interface NameAndEmail {
+
+// interface NameAndEmail {
+//   name: string;
+//   email: string;
+// }
+
+// interface User extends NameAndEmail {
+//   id: string;
+//   role: string;
+// }
+
+// const fetchUser = async (): Promise<NameAndEmail> => {
+//   const response = await fetch("/api/user");
+//   const user = await response.json();
+//   return user;
+// };
+
+// const example = async () => {
+//   const user = await fetchUser();
+
+//   type test = Expect<Equal<typeof user, { name: string; email: string }>>;
+// };
+
+interface User {
+  id: string;
   name: string;
   email: string;
-}
-
-interface User extends NameAndEmail {
-  id: string;
   role: string;
 }
 
-const fetchUser = async (): Promise<NameAndEmail> => {
+type PickedUser = Pick<User, "name" | "email">;
+
+const fetchUser = async (): Promise<PickedUser> => {
   const response = await fetch("/api/user");
   const user = await response.json();
   return user;
