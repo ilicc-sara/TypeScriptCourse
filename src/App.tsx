a8b54faf6de49e4f3ccdcc95a4a581ba568a124c
Sara <sarailic431@gmail.com>
2025-12-28 22:51:37 +0100

    improve type interference for an async function

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,29 @@ import type { Task } from "./types";
 import { serialize } from "v8";
 import "@total-typescript/ts-reset";
 
+// ///////////////// video 173
+
+const fetchData = async () => {
+  const result = await fetch("/");
+
+  if (!result.ok) {
+    return [new Error("Could not fetch data.")];
+  }
+
+  const data = await result.json();
+
+  return [undefined, data];
+};
+
+const example = async () => {
+  const [error, data] = await fetchData();
+
+  type Tests = [
+    Expect<Equal<typeof error, Error | undefined>>,
+    Expect<Equal<typeof data, any>>
+  ];
+};
+
 // ///////////////// video 172
 
 const users = ["matt", "sofia", "waqas"] as const;
