295081974234f699110d2cce54ab3cb134feec9f
Sara <sarailic431@gmail.com>
2025-12-29 22:17:28 +0100

    classes in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,23 @@ import type { Task } from "./types";
 import { serialize } from "v8";
 import "@total-typescript/ts-reset";
 
+// ///////////////// video 176
+
+class CanvasNode {}
+
+it("Should store some basic properties", () => {
+  const canvasNode = new CanvasNode();
+
+  expect(canvasNode.x).toEqual(0);
+  expect(canvasNode.y).toEqual(0);
+
+  // @ts-expect-error Property is readonly
+  canvasNode.x = 10;
+
+  // @ts-expect-error Property is readonly
+  canvasNode.y = 20;
+});
+
 // ///////////////// video 175
 
 type ButtonAttributes = {
